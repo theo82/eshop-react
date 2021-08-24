3417bc3822a266509166425933e9d7085639ba55
theo82 <theoaristi53@gmail.com>
2021-08-24 17:52:11 +0300

    React Router - Setup

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-import styled from 'styled-components'
-import Testing from './testing'
-const Button = styled.button`
-  background: green;
-  color: white;
-`
-const Container = styled.div`
-  background: red;
-  color: white;
-  .hero {
-    font-size: 8rem;
-  }
-`
-
-const Container2 = styled.div`
-  background: red;
-  color: white;
-  .hero {
-    font-size: 1rem;
-  }
-`
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from './pages'
 
 function App() {
   return (
-    <div>
-      <h4>comfy sloth starter</h4>
-      <Button>click me</Button>
-      <Container>
-        <div>
-          <h3>Hello world</h3>
-        </div>
-        <div className='hero'>hero text</div>
-      </Container>
-      <Container2>
-        <div>
-          <h3>Hello world</h3>
-        </div>
-        <div className='hero'>hero text</div>
-      </Container2>
-      <Testing />
-    </div>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProduct />} />
+        <Route exact path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   )
 }
 
