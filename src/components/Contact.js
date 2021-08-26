f7eafd1f3673cbb8c9b6417d80b877ea1b01e579
theo82 <theoaristi53@gmail.com>
2021-08-26 18:26:10 +0300

    Formspree

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,37 @@
 import React from 'react'
 import styled from 'styled-components'
+import Services from './Services'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum,
+            dolorum.
+          </p>
+          <form
+            className='contact-form'
+            action='https://formspree.io/f/xgerpgvy'
+            method='POST'
+          >
+            <input
+              type='email'
+              className='form-input'
+              placeholder='enter email'
+              name='_replyto'
+            ></input>
+
+            <button type='submit' className='submit-btn'>
+              subscibe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
