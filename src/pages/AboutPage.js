0bc8373655362fd25499b3fd22be83d50b30a44b
theo82 <theoaristi53@gmail.com>
2021-08-26 08:59:39 +0300

    About, Checkout Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,26 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our title</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            explicabo reiciendis, molestiae dignissimos nostrum, aliquam iste
+            eos vero voluptatum ducimus ipsum ratione, saepe perspiciatis.
+            Obcaecati ratione repellat eligendi perferendis beatae.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
