7c1d2551c0b1bd9c5e525c678ab0139b3d8f8dfe
theo82 <theoaristi53@gmail.com>
2021-08-30 10:26:44 +0300

    Single Product - Stars - array - approach

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -3,58 +3,31 @@ import styled from 'styled-components'
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
 const Stars = ({ stars, reviews }) => {
   console.log(stars, reviews)
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5
+    return (
+      <span>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= number ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    )
+  })
   return (
+    // <span>
+    //       {stars >= 1 ? (
+    //         <BsStarFill />
+    //       ) : stars >= 0.5 ? (
+    //         <BsStarHalf />
+    //       ) : (
+    //         <BsStar />
+    //       )}
     <Wrapper>
-      <div className='stars'>
-        {}
-        <span>
-          {stars >= 1 ? (
-            <BsStarFill />
-          ) : stars >= 0.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-          {stars >= 2 ? (
-            <BsStarFill />
-          ) : stars >= 1.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-          {stars >= 3 ? (
-            <BsStarFill />
-          ) : stars >= 2.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-          {stars >= 4 ? (
-            <BsStarFill />
-          ) : stars >= 3.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-          {stars === 5 ? (
-            <BsStarFill />
-          ) : stars >= 4.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {}
-        {}
-        <span>
-          {stars === 5 ? (
-            <BsStarFill />
-          ) : (
-            (stars) => (4.5 ? <BsStarHalf /> : <BsStar />)
-          )}
-        </span>
-        {}
-      </div>
+      <div className='stars'>{tempStars}</div>
       <p className='reviews'>({reviews}) customer reviews</p>
     </Wrapper>
   )
