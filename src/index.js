290ee98cd1bd5643e4ebb1e82a3780aee088e668
theo82 <theoaristi53@gmail.com>
2021-09-17 07:51:30 +0300

    Auth0 - Provider

--- src/index.js
+++ src/index.js
@@ -8,13 +8,26 @@ import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+/**
+ *  domain: dev-ytjg1xp2.us.auth0.com
+ *  client_id: HcX3tkWvfWeWL8iYAkvqCvJrI7WZYdyB
+ * */
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain='dev-ytjg1xp2.us.auth0.com'
+    clientId='HcX3tkWvfWeWL8iYAkvqCvJrI7WZYdyB'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+    ,
+  </Auth0Provider>,
   document.getElementById('root')
 )
