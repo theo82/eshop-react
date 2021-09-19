051a8892214bd1664f4c9f87381445f61f4b7a63
theo82 <theoaristi53@gmail.com>
2021-09-19 21:01:33 +0300

    Auth0 - Hide Checkout

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom'
 
 const CartTotals = () => {
   const { total_amount, shipping_fee } = useCartContext()
+  const { myUser, loginWithRedirect } = useUserContext()
   console.log('total_amount: ' + total_amount)
   return (
     <Wrapper>
@@ -24,6 +25,15 @@ const CartTotals = () => {
             <span>{formatPrice(total_amount + shipping_fee)}</span>
           </h4>
         </article>
+        {myUser ? (
+          <Link to='/checkout' className='btn'>
+            proceed to checkout
+          </Link>
+        ) : (
+          <button type='button' className='btn' onClick={loginWithRedirect}>
+            login
+          </button>
+        )}
       </div>
     </Wrapper>
   )
