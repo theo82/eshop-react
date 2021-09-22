a9f555caaddbbd38361e27c5f3399362a60e7915
theo82 <theoaristi53@gmail.com>
2021-09-22 09:10:44 +0300

    Stripe Checkout - Setup

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -8,7 +8,7 @@ import { useUserContext } from '../context/user_context'
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext()
-  const { total_items } = useCartContext()
+  const { total_items, clearCart } = useCartContext()
   const { loginWithRedirect, myUser, logout } = useUserContext()
 
   return (
@@ -25,6 +25,7 @@ const CartButtons = () => {
           type='button'
           className='auth-btn'
           onClick={() => {
+            clearCart()
             logout({ returnTo: window.location.origin })
           }}
         >
